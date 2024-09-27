b90d44aed11dd6362c8b0adc87d846034e61cdca
diane <villalino.136534100519@depedqc.ph>
2024-09-27 11:21:47 +0800

    Display the choices for calculation (I, V, R)

--- ohms_law_calculator.py
+++ ohms_law_calculator.py
@@ -1,3 +1,5 @@
 # State the program
 print("Ohm's Law Calculator")
 # Provide the program's functions
+print("What would you like to calculate?")
+print("1. Current (I)\n2. Voltage (V)\n3. Resistance (R)")
